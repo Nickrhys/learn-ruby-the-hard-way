2010614c3d8beb934269c2a2aa1c96436ea898bf
Nickrhys <nick@red56.uk>
2015-06-12 15:34:59 +0100

    exercise 17

--- ex16.rb
+++ ex16.rb
@@ -1,16 +1,16 @@
 filename = ARGV.first
 
-# puts "We're going to erase #{filename}"
-# puts "If you don't want that, hit CTRL-C (^C)."
-# puts "If you do what that, hit RETURN."
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do what that, hit RETURN."
 
-# $stdin.gets
+$stdin.gets
 
 puts "Opening the file..."
 target = open(filename, 'w+')
 
-# puts "Truncating the file. Goodbye!"
-# target.truncate(5)
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
 
 puts "Now I'm going to ask you for three lines"
 
@@ -38,3 +38,9 @@ print target.read
 
 puts "And finally, we close it."
 target.close
+
+puts "let's read it one more time"
+
+reader = open(filename)
+
+print reader.read
